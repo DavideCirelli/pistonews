18209a0ae16c7d077579c082d42d10f841678767
DavideCirelli <125650164+DavideCirelli@users.noreply.github.com>
2025-10-05 16:38:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, session, flash, send_from_directory, Response
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import os
